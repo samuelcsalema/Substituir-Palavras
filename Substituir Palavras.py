05ae0d23175804291782156ce8262c28da40520a
Samuel <samuelcsalema2@gmail.com>
2025-07-02 13:41:06 -0300

    Corrigindo erro

--- Substituir Palavras.py
+++ Substituir Palavras.py
@@ -3,6 +3,6 @@ localizar = input('Localizar: ')
 substituta = input('Substituir: ')
 
 for i in range(len(texto)):
-    texto[i] = texto[i].replace(original, localizar)
+    texto[i] = texto[i].replace(localizar, substituta)
     
 print(' '.join(texto))
