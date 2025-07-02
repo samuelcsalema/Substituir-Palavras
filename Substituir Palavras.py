84aee352fca794ba9ac5b8c1c22335d4a26ecb31
Samuel <samuelcsalema2@gmail.com>
2025-07-02 13:39:57 -0300

    Atualizando nomes

--- Substituir Palavras.py
+++ Substituir Palavras.py
@@ -1,9 +1,8 @@
 texto = input('Texto: ').split()
-original = input('Palavra que deseja substituir: ')
-substituta = input('Palavra para substituir: ')
-
+localizar = input('Localizar: ')
+substituta = input('Substituir: ')
 
 for i in range(len(texto)):
-    texto[i] = texto[i].replace(original, substituta)
+    texto[i] = texto[i].replace(original, localizar)
     
 print(' '.join(texto))
